bd5afe8cdb63b1377c14a6fa0efe4d09aa32d9c9
sbhalrhu <sahibbhalrhu@gmail.com>
2026-08-30 23:24:34 -0700

    lab0pt1

---
+++ who_runs_first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#define ITERATIONS 1000
+
+int main(void)
+{
+    char parentText[] = "parent";
+    char childText[] = "child";
+    char newLine[] = "\n";
+
+    for (int i = 0; i<ITERATIONS; i++) 
+    {
+        pid_t pid = fork();
+        if (pid) {
+            write(STDOUT_FILENO, parentText, sizeof(parentText)-1);
+            write(STDOUT_FILENO, newLine, sizeof(newLine)-1);
+            wait(NULL);
+        } else {
+            write(STDOUT_FILENO, childText, sizeof(childText)-1);
+            write(STDOUT_FILENO, newLine, sizeof(newLine)-1);
+            return 0;
+        }
+    }
+}
