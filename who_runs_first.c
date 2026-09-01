3a2f67743fcc6dfc080a49fdb91b4fe5c2139126
sbhalrhu <sahibbhalrhu@gmail.com>
2026-09-01 11:33:35 -0700

    write with 0.1s sleep

--- who_runs_first.c
+++ who_runs_first.c
@@ -3,7 +3,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #define ITERATIONS 1000
-#define SLEEP 1.0
+#define SLEEP 0.1 
 
 int main(void)
 {
@@ -15,19 +15,15 @@ int main(void)
     {
         pid_t pid = fork();
         if (pid) {
-            /*
+            
             sleep(SLEEP); 
             write(STDOUT_FILENO, parentText, sizeof(parentText)-1);
-            */
-            printf("parent");
-            fflush(stdout);
+            
             wait(NULL);
         } else {
-            /*sleep(SLEEP);
+            sleep(SLEEP);
             write(STDOUT_FILENO, childText, sizeof(childText)-1);
-            */
-            printf("child");
-            fflush(stdout);
+            
             return 0;
         }
         /*write(STDOUT_FILENO, newLine, sizeof(newLine)-1);*/
